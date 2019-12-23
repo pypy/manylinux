022979ea434021acd4777ec75ef602fc4aa13664
Yannick Jadoul <yannick.jadoul@belgacom.net>
2019-12-23 16:09:45 +0100

    Adding 7.3.0rc3

--- docker/build_scripts_pypy/prefetch_pypy.sh
+++ docker/build_scripts_pypy/prefetch_pypy.sh
@@ -3,8 +3,10 @@
 set -ex
 
 SOURCES=docker/sources
-BITBUCKET_URL=https://bitbucket.org/squeaky/portable-pypy/downloads # old releases
-URL=https://github.com/squeaky-pl/portable-pypy/releases/download # new releases
+SQUEAKY_BITBUCKET_URL=https://bitbucket.org/squeaky/portable-pypy/downloads # older releases
+SQUEAKY_GITHUB_URL=https://github.com/squeaky-pl/portable-pypy/releases/download # old releases
+URL=https://bitbucket.org/pypy/pypy/downloads # new releases
+
 
 MY_DIR=$(dirname "${BASH_SOURCE[0]}")
 . $MY_DIR/../build_scripts/build_utils.sh
@@ -13,9 +15,13 @@ MY_DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$SOURCES"
 
 # pypy 7.1.1
-fetch_source pypy-7.1.1-linux_x86_64-portable.tar.bz2 "$BITBUCKET_URL"
-fetch_source pypy3.6-7.1.1-beta-linux_x86_64-portable.tar.bz2 "$BITBUCKET_URL"
+fetch_source pypy-7.1.1-linux_x86_64-portable.tar.bz2 "$SQUEAKY_BITBUCKET_URL"
+fetch_source pypy3.6-7.1.1-beta-linux_x86_64-portable.tar.bz2 "$SQUEAKY_BITBUCKET_URL"
 
 # pypy 7.2.0
-fetch_source pypy-7.2.0-linux_x86_64-portable.tar.bz2 "$URL/pypy-7.2.0"
-fetch_source pypy3.6-7.2.0-linux_x86_64-portable.tar.bz2 "$URL/pypy3.6-7.2.0"
+fetch_source pypy-7.2.0-linux_x86_64-portable.tar.bz2 "$SQUEAKY_GITHUB_URL/pypy-7.2.0"
+fetch_source pypy3.6-7.2.0-linux_x86_64-portable.tar.bz2 "$SQUEAKY_GITHUB_URL/pypy3.6-7.2.0"
+
+# pypy 7.3.0rc3
+fetch_source pypy2.7-v7.3.0rc3-linux64.tar.bz2 "$URL"
+fetch_source pypy3.6-v7.3.0rc3-linux64.tar.bz2 "$URL"
