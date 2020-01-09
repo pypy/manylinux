7403084977ab8c65249ba97149d955a0f476d5a3
Dejan Lekic <dejanlekic@bmlltech.com>
2020-01-09 14:55:32 +0000

    Now we fetch 7.3.0 too

--- docker/build_scripts_pypy/prefetch_pypy.sh
+++ docker/build_scripts_pypy/prefetch_pypy.sh
@@ -21,5 +21,7 @@ fetch_source pypy-7.2.0-linux_x86_64-portable.tar.bz2 "$URL/pypy-7.2.0"
 fetch_source pypy3.6-7.2.0-linux_x86_64-portable.tar.bz2 "$URL/pypy3.6-7.2.0"
 
 # pypy 7.3.0
-fetch_source pypy-7.3.0-linux_x86_64-portable.tar.bz2 "$URL/pypy-7.3.0"
-fetch_source pypy3.6-7.3.0-linux_x86_64-portable.tar.bz2 "$URL/pypy3.6-7.3.0"
+# Since 7.3.0 pypy is building portable binary packages, so we can safely grab them from there
+URL=https://bitbucket.org/pypy/pypy/downloads
+fetch_source pypy2.7-v7.3.0-linux64.tar.bz2 "$URL"
+fetch_source pypy3.6-v7.3.0-linux64.tar.bz2 "$URL"
